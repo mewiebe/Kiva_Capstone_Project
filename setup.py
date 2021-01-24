685c8b70d28b957914609fc741749b974fae85ba
mewiebe <mwalz2@gmail.com>
2021-01-23 19:20:55 -0800

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Capstone project to predict which microloans on the Kiva platform will be funded and how quickly this will happen.',
+    author='Maureen Wiebe',
+    license='MIT',
+)
